59357fd70691bd3040c9a7c0b6b6c669c68b09ee
Samuel <samuel@seedlegals.com>
2024-04-05 11:18:52 +0100

    initial commit

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -3,19 +3,18 @@ import { Link } from "react-router-dom";
 import "./HomePage.css";
 
 export const HomePage = () => {
-  return (
-    <div className="home">
-      <div className="left-half">
-        <h1>Welcome to Acebook!</h1>
-        <h2>Login to Your Account</h2>
-        <Link className="login" to="/login">Log In</Link>
-      </div>
-      <div className="right-half">
-        <h3>New Here?</h3>
-        <p>Sign up and start sharing moments with your friends today! Join our community and explore endless possibilities together.</p>
-        <Link className="signup" to="/signup">Sign Up</Link>
-      </div>
-    </div>
-  );
+    return (
+        <div className="home">
+            <div className="welcome-message">
+                <h1>Welcome to Cappybook!</h1>
+            </div>
+            <div className="signup-section">
+                <div className="button-container">
+                    <Link className="login" to="/login">Log In</Link>
+                    <Link className="signup" to="/signup">Sign Up</Link>
+                </div>
+            </div>
+        </div>
+    );
 };
 
